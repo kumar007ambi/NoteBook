74214db973106a4e0096951d11fd159a7899a97c
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-05-28 20:40:49 +0530

    resolve conflicts for navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -32,7 +32,30 @@ function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-             
+                {/* Here Two li element should be removed */}
+            <li className="nav-item">
+                  <Link
+                    to="/notes"
+                    className={`nav-link ${
+                      location.pathname === "/notes" ? "active" : ""
+                    }`}
+                    aria-current="page"
+                  >
+                    Your Notes
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link
+                    to="/"
+                    className={`nav-link ${
+                      location.pathname === "/" ? "active" : ""
+                    }`}
+                    aria-current="page"
+                  >
+                    AddNote
+                  </Link>
+                </li>
               <li className="nav-item">
                 <Link
                   to="/about"
