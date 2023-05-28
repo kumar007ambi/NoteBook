5ae7c6b3b88840db2688ebea25d78840e0a82c33
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2023-05-28 20:47:28 +0530

    resolve conflicts for navbar again

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -32,30 +32,31 @@ function Navbar() {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                {/* Here Two li element should be removed */}
-            <li className="nav-item">
-                  <Link
-                    to="/notes"
-                    className={`nav-link ${
-                      location.pathname === "/notes" ? "active" : ""
-                    }`}
-                    aria-current="page"
-                  >
-                    Your Notes
-                  </Link>
-                </li>
+              {/* These 2 li should be removed */}
+              <li className="nav-item">
+                <Link
+                  to="/notes"
+                  className={`nav-link ${
+                    location.pathname === "/notes" ? "active" : ""
+                  }`}
+                  aria-current="page"
+                >
+                  Your Notes
+                </Link>
+              </li>
 
-                <li className="nav-item">
-                  <Link
-                    to="/"
-                    className={`nav-link ${
-                      location.pathname === "/" ? "active" : ""
-                    }`}
-                    aria-current="page"
-                  >
-                    AddNote
-                  </Link>
-                </li>
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className={`nav-link ${
+                    location.pathname === "/" ? "active" : ""
+                  }`}
+                  aria-current="page"
+                >
+                  AddNote
+                </Link>
+              </li>
+
               <li className="nav-item">
                 <Link
                   to="/about"
@@ -67,8 +68,8 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
-            {/* This li appears after the successfull login */}
             {localStorage.getItem("token") ? (
+              // These works for the button after user login it appears only then
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
                   <Link
@@ -119,37 +120,36 @@ function Navbar() {
               </form>
             ) : (
               <>
-                
                 {localStorage.getItem("name") ? (
                   <div className="h3 px-10">{localStorage.getItem("name")}</div>
                 ) : (
                   ""
-                )}<Link
-                to="/login"
-                onClick={handleLogout}
-                className={`nav-link px-3 ${
-                  location.pathname === "/logout" ? "active" : ""
-                }`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  fill="currentColor"
-                  class="bi bi-box-arrow-right h3"
-                  viewBox="0 0 16 16"
+                )}
+                <Link
+                  to="/login"
+                  onClick={handleLogout}
+                  className={`nav-link px-3 ${
+                    location.pathname === "/logout" ? "active" : ""
+                  }`}
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-                  />
-                </svg>
-              </Link>
-
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="25"
+                    fill="currentColor"
+                    class="bi bi-box-arrow-right h3"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+                    />
+                  </svg>
+                </Link>
               </>
             )}
           </div>
